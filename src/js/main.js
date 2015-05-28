dee597d3e787dd9c3614f4efd1df765bdb3f3b56
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-05-28 15:24:15 -0500

    added bet functions and opponent cards

--- src/js/main.js
+++ src/js/main.js
@@ -2,23 +2,39 @@ var API_URL  = "http://deckofcardsapi.com/api/shuffle/?deck_count=";
 var DRAW_URL = "http://deckofcardsapi.com/api/draw/";
 var back     = "/img/card-back.jpg";
 var deck_id;
-var status = "";
+var status;
+var cash = 100;
+var bet = 0;
+var replace = [];
 
+//buttons
 var $newGame = $(".new-game");
 var $dealButton = $(".deal");
 var $fold = $('.fold');
+var $form = $('form');
+var $betInput = $('.bet-input');
 var $check = $(".check");
-var $community = $(".community-cards");
-var $hole = $(".hole-cards")
+var $bet = $(".bet");
+var $call = (".call");
+var $raise = $(".raise");
 
+//divs
+var $community = $(".community-cards");
+var $hole = $(".hole-cards");
+var $opponent = $(".opponent");
+
+//appends starting cash and bet on load
+addBetToPage();
 
 $newGame.click(function(){
-  newDecks(6);
+  newDecks(1);
   $dealButton.removeAttr("disabled");
+  status = "";
 });
 
 $dealButton.click(function(){
   drawCards(2, $hole);
+  drawCards(2, $opponent);
   $dealButton.attr("disabled", "disabled");
 })
 
@@ -27,21 +43,20 @@ $fold.click(function(){
   $hole.empty();
 })
 
-$check.click(function(){
-  switch(status){
-    case "":
-    drawCards(3, $community);
-    status = "turn";
-    break;
-    case "turn":
-    drawCards(1, $community);
-    status = "river";
-    break;
-    case "river":
-    drawCards(1, $community);
-    status = "finished";
-    break;
+$form.submit(function(){
+  if($betInput.val() <= cash){
+    bet += parseInt($betInput.val());
+    addBetToPage();
+    dealCommunity();
   }
+  else{
+    alert("You don't have enough money!");
+  }
+  event.preventDefault();
+})
+
+$check.click(function(){
+  dealCommunity();
 })
 
 // get new decks
@@ -63,11 +78,51 @@ function drawCards(card_count, target, callback){
     }
 }
 
+//append cards to page face up for player face down for opponent
 function appendCards(data, target){
   data.cards.forEach(function(card){
-      var card = card.image==="http://deckofcardsapi.com/static/img/AD.png" ? "../img/aceDiamond.png" : card.image
-      target.append("<img src="+ card +"></img>")
-    });
+    var card = card.image==="http://deckofcardsapi.com/static/img/AD.png" ? "../img/aceDiamond.png" : card.image;
+    if(target===$opponent){
+      replace.push(card);
+      target.append("<img src="+ back +"></img>");
+    }else{
+      target.append("<img src="+ card +"></img>");
+    }
+  });
+}
+
+//deals community cards depending on status
+function dealCommunity(){
+  switch(status){
+    case "":
+    drawCards(3, $community);
+    status = "turn";
+    break;
+    case "turn":
+    drawCards(1, $community);
+    status = "river";
+    break;
+    case "river":
+    drawCards(1, $community);
+    status = "finished";
+    revealOpponent();
+    break;
+  }
+}
+
+//reveals opponents face down cards
+function revealOpponent(){
+  $opponent.empty();
+  replace.forEach(function(card){
+    $opponent.append("<img src="+ card +"></img>");
+  })
+}
+
+
+//adds bet to page
+function addBetToPage(){
+  $(".cash-space").text("$"+cash);
+  $(".current-bet-space").text("$"+bet);
 }
 
 //scott's magic function for this api
